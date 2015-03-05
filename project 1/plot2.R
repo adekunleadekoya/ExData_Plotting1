20c36515a38c57475627c7f0029225dfb2690660
Adekunle Adekoya <adekunleadekoya@gmail.com>
2015-03-05 23:47:43 +0100

    commit

--- project 1/plot2.R
+++ project 1/plot2.R
@@ -1,6 +1,6 @@
 
-source("load_data.R") # include load_data.R in this file
-plot2<- function() 
+ 
+	plot2<- function() 
 	{
 		 
 		data <- load_data() # loads data
@@ -8,6 +8,27 @@ plot2<- function()
 		plot(data$Time, data$Global_active_power,
 		type="l",xlab="",ylab="Global Active Power (kilowatts)")
 		dev.off()
+	}
+
+	load_data <- function()
+	 {
+		file.name = "household_power_consumption.txt" # sets filename
+
+		df <- read.table(file.name,header=TRUE,sep=";",
+				colClasses=c("character", "character",
+				 rep("numeric",7)),na="?") # populates data frame
+
+		# convert date and time variables to Date/Time class
+		df$Time <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
+
+		#coerce df$Time to Date class
+		df$Date <- as.Date(df$Date, "%d/%m/%Y")
+
+		# only use data from the dates 2007-02-01 and 2007-02-02
+		dates <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
+
+		df <- subset(df, Date %in% dates) # subsets df
+		return(df)
 	}
 
 	plot2()  # calls above function so as to plot graph
